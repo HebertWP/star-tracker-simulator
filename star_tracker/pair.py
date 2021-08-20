2c5706050a4686c670e34dd5d05a5a011fd2af88
Hebert W Parreira <wandick.hebert98@gmail.com>
2021-08-20 13:12:02 -0300

    pair class added

--- star_tracker/pair.py
+++ star_tracker/pair.py
@@ -1,8 +1,30 @@
 class Pair:
+    #init
     def __init__(self,first_star_ID,second_starID,angle):
         self.first_star_ID=first_star_ID
         self.second_starID=second_starID
         self.angle=angle
+    
+    def append(self,next):
+        if hasattr(self, "next") :
+            next.append(self.next)
+        self.next=next
+
+    def orderedInsertion(self,element):
+        if element < self:
+            element.next = self
+            return element
+        if hasattr(self, "next"):
+            self.next=self.next.orderedInsertion(element)
+        else :
+            self.next=element
+        return self
+        
+    def __str__(self):
+        s = ' %d --> %d %d,' % (self.first_star_ID, self.second_starID, self.angle)
+        if hasattr(self, "next"):
+            s = s + str(self.next) 
+        return s
     
     #minor than
     def __lt__(self,other):
@@ -12,9 +34,14 @@ class Pair:
     def __le__(self,other):
         return self.angle <= other.angle
 
+    #equal to
     def __eq__(self, other):
         return self.angle == other.angle
     
+    #bigger equal to
+    def __ge__(self,other):
+        return self.angle >= other.angle
+
     #bigger than
     def __gt__(self, other):
         return self.angle > other.angle
--- tests/test_pair.py
+++ tests/test_pair.py
@@ -1,4 +1,4 @@
-from star_tracker.star import Pair
+from star_tracker.pair import Pair
 
 def test_minor_than():
     p1 = Pair(0,1,2)
@@ -21,9 +21,51 @@ def test_equal_to():
 
     assert res == False
 
+def test_equal_bigger_to():
+    p1 = Pair(0,1,2)
+    p2 = Pair(0,1,3)
+    res = p1 >= p2
+
+    assert res == False
+
 def test_bigger_than():
     p1 = Pair(0,1,2)
     p2 = Pair(0,1,3)
     res = p1 > p2
 
     assert res == False
+
+def test_print():
+    p = Pair(0,1,2)
+    s = str(p)
+    assert s == ' 0 --> 1 2,'
+
+def test_append():
+    p1 = Pair(0,1,2)
+    p2 = Pair(0,2,3)
+    p1.append(p2)
+    s = str(p1)
+    assert s == ' 0 --> 1 2, 0 --> 2 3,'
+
+def test_insert_before():
+    p1 = Pair(0,1,3)
+    p2 = Pair(0,2,1)
+    p1=p1.orderedInsertion(p2)
+    s = str(p1)
+    assert s == ' 0 --> 2 1, 0 --> 1 3,'
+
+def test_insert_last_position():
+    p1 = Pair(0,1,3)
+    p2 = Pair(0,2,5)
+    p1=p1.orderedInsertion(p2)
+    s = str(p1)
+    assert s == ' 0 --> 1 3, 0 --> 2 5,'
+
+def test_insert_in_middle_position():
+    p1 = Pair(0,1,3)
+    p2 = Pair(0,2,5)
+    p3 = Pair(1,8,4)
+    p1=p1.orderedInsertion(p2)
+    p1=p1.orderedInsertion(p3)
+    s = str(p1)
+    assert s == ' 0 --> 1 3, 1 --> 8 4, 0 --> 2 5,'
