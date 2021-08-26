d7515a2e6d7afa8bc30cfd3314b2d478280adac1
Hebert W Parreira <wandick.hebert98@gmail.com>
2021-08-26 10:57:49 -0300

    minor bug correction and number of star reduced

--- star_tracker/datagenerator.py
+++ star_tracker/datagenerator.py
@@ -4,7 +4,7 @@ import csv
 def creteRandomStarData(outputFile, minMag, maxMag, starNumbers):
     file=open(outputFile,"w")
     fieldnames = ['Numero de catalogacao', 'phi', 'theta', 'Magnitude visual']
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer = csv.DictWriter(file, fieldnames = fieldnames)
     writer.writeheader()
-    for x in range(10000):
+    for x in range(starNumbers):
         writer.writerow({'Numero de catalogacao':x, 'phi':random.uniform(0,360), 'theta':random.uniform(0,180),'Magnitude visual':random.uniform(minMag,maxMag)})
--- star_tracker/loadfile.py
+++ star_tracker/loadfile.py
@@ -1,11 +1,16 @@
 import pandas
-import numpy as np
+from numpy import sin,cos,pi
+import matplotlib
+try:
+    from star_tracker.basic import *
+except ImportError:
+    from basic import *
 
 def loadRawData(inputfile):
     stars = pandas.read_csv(inputfile)
     
-    THETA = stars["theta"]*(np.pi/180)
-    PHI = stars["phi"]*(np.pi/180)
+    THETA = stars["theta"]*(pi/180)
+    PHI = stars["phi"]*(pi/180)
     MAG = stars["Magnitude visual"]
     NAME = stars["Numero de catalogacao"]
     
@@ -18,3 +23,34 @@ def loadRawData(inputfile):
         mag.append(MAG[i])
         name.append(NAME[i])
     return name, theta, phi, mag
+
+def plotCatalog2D(theta,phi, mag, plt,output):
+    plt.figure()
+    mag = [num/10 for num in mag]
+    ax = plt.axes()
+    ax.set_facecolor("k")
+    plt.scatter(phi,theta, s=mag,c='white')
+    plt.title("2-Dimensional Star Catalog")
+    plt.xlabel("phi [rad]")
+    plt.ylabel("theta [rad]")
+    plt.savefig(output)
+    plt.close('all')
+    
+
+def plot3D(theta, phi, mag, plt, output):
+    x,y,z = spherical2catersian(theta,phi)
+    mag = [num /10 for num in mag]
+    plt.figure()
+    ax = plt.axes(projection ="3d")
+    plt.gca().set_facecolor('k')
+    ax.set_axis_off()
+    # Creating plot
+    ax.scatter3D(x, y, z, s = mag, color = "white")
+    ax.scatter3D(0, 0, 0, s = 1,color = "red")
+        
+    #ax.view_init(elev=0, azim=0)
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    plt.savefig(output)
+    plt.close('all')
