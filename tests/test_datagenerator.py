8cee09ffefb2c30bc39a5c6a714c9ecde4cbe9c8
Hebert W Parreira <wandick.hebert98@gmail.com>
2021-09-08 11:24:30 -0300

    remove old random generator

--- tests/test_datagenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from star_tracker.datagenerator import creteRandomStarData
-
-def test_creteRandomStarData():
-    random.seed(10)
-    creteRandomStarData("data/stars.csv", 4, 7, 1500)
-
-    assert True == True
